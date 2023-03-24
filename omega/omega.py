a0fabe1a401bdb50cdaebda47b77b333dd9b2b80
gennaron <noegennaro73@gmail.com>
2023-03-25 00:01:51 +0100

    add optional for campus_id

--- omega/omega.py
+++ omega/omega.py
@@ -125,7 +125,7 @@ async def ping(interaction: Interaction):
     app_commands.Choice(name = 'coa', value = 3),
     app_commands.Choice(name = 'years', value = 4),
 ])
-async def sync(interaction: Interaction,type: app_commands.Choice[int], intra_id: int, role_id: str, campus_id: int):
+async def sync(interaction: Interaction,type: app_commands.Choice[int], intra_id: int, role_id: str, campus_id: int=0):
     level = await admin_check(interaction.user.id)
     if (not interaction.user.guild_permissions.administrator and level <= 2):
         await interaction.response.send_message(f"Not allowed !\nYou must be administrator", ephemeral = True)
@@ -150,7 +150,7 @@ async def sync(interaction: Interaction,type: app_commands.Choice[int], intra_id
     app_commands.Choice(name = 'Allow', value = 1),
     app_commands.Choice(name = 'Deny', value = 0),
 ])
-async def sync_project(interaction: Interaction, intra_id: int, in_progress: app_commands.Choice[int], finished: app_commands.Choice[int], validated: app_commands.Choice[int], role_id: str, campus_id: int):
+async def sync_project(interaction: Interaction, intra_id: int, in_progress: app_commands.Choice[int], finished: app_commands.Choice[int], validated: app_commands.Choice[int], role_id: str, campus_id: int=0):
     level = await admin_check(interaction.user.id)
     if (not interaction.user.guild_permissions.administrator and level <= 2):
         await interaction.response.send_message(f"Not allowed !\nYou must be administrator", ephemeral = True)
@@ -163,7 +163,7 @@ async def sync_project(interaction: Interaction, intra_id: int, in_progress: app
 
 @tree.command(name = "nick", description = "set the nick parameters on the sever (&login and &campus works)")
 @app_commands.guild_only()
-async def nick(interaction: Interaction,namming_pattern: str,campus_id: int):
+async def nick(interaction: Interaction,namming_pattern: str, campus_id: int=0):
     level = await admin_check(interaction.user.id)
     if (not interaction.user.guild_permissions.administrator and level <= 2):
         await interaction.response.send_message(f"Not allowed !\nYou must be administrator", ephemeral = True)
