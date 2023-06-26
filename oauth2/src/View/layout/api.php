1c0170184508cfc6e78004a91ac5903c77f3db90
Pillo <antoine@42lyon.fr>
2023-06-26 19:59:12 +0200

    fix(oauth2): remove print of undefined var

--- oauth2/src/View/layout/api.php
+++ oauth2/src/View/layout/api.php
@@ -5,6 +5,6 @@ $pageName = 'Sign in';
 <div class="page">
 	<p>&gt; Protocole Omega &lt;</p>
 	<a href="<?= $data['url_intra'] ?>" class="but">
-		LOGIN INTRA 42 <?= $value ?>
+		LOGIN INTRA 42
 	</a>
 </div>
